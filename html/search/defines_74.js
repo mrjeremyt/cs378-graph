89386145c0dd62e53f37fcd136e0e2e21f7ccc72
mrjeremyt <jthompson2214@gmail.com>
2014-08-04 15:22:09 -0500

    pushing newly ran doxygen stuff.

---
+++ html/search/defines_74.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['test_5ftypes',['TEST_TYPES',['../jjc2828-TestGraph_8c_09_09.html#aa21c996aadce8ae75772cbc3b8793842',1,'jjc2828-TestGraph.c++']]]
+];
--- html/search/related_6f.js
+++ html/search/related_6f.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['operator_2b',['operator+',['../classGraph_1_1vertex__iterator.html#a4274bad156529dd9c10d3b8fe032212d',1,'Graph::vertex_iterator']]],
+  ['operator_2d',['operator-',['../classGraph_1_1vertex__iterator.html#ae83a65c3618bbe7e482e15535c39af43',1,'Graph::vertex_iterator']]],
   ['operator_3d_3d',['operator==',['../classGraph_1_1vertex__iterator.html#a6bb0446817568dc65a765ae027d95306',1,'Graph::vertex_iterator::operator==()'],['../classGraph_1_1adjacency__iterator.html#a0a6f6970cd47d7f7782bd65570c1a389',1,'Graph::adjacency_iterator::operator==()'],['../classGraph_1_1edge__iterator.html#a2605fddf9cddc4ad87f20db3ef7990d1',1,'Graph::edge_iterator::operator==()']]]
 ];
